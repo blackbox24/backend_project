0bcad274648eaf913f55588b828218c573c62a8a
blackbox24 <nicardid@gmail.com>
2025-07-01 17:02:14 +0000

    feats: implemented delete, update, mark-in-progres, list.. and help

--- task_tracker_cli/main.py
+++ task_tracker_cli/main.py
@@ -28,7 +28,13 @@ class ListDataModel:
             self.updatedAt = str(datetime.now())
         else:
             self.updatedAt = datetime.now()
-    
+
+    def __doc__(self):
+        doc = open("docs.txt","r")
+        _help = doc.read()
+        doc.close()
+        logger.info(f"Documentation\n{_help}")
+
     def load_data(self):
         try:
             file = open("data.json","+r")
@@ -36,25 +42,25 @@ class ListDataModel:
             file.close()
 
             logger.info("Data loaded successfully")
-            logger.info(f"Data: {data}")
-
-            file = open("data.json","w+")
-
 
         except json.decoder.JSONDecodeError as e:
             logger.warning(str(e))
             data = []
-        return data,file
+        return data
+
+    def write_data(self):
+        file = open("data.json","w+")
+        return file
     
-    def save(self,data,file):
-        
-        data.append({
-            "id":self.id,
-            "description": self.description,
-            "status": self.status,
-            "createdAt": self.createdAt,
-            "updatedAt": self.updatedAt
-        })
+    def save(self,data,file,update=None):
+        if update == None:
+            data.append({
+                "id":self.id,
+                "description": self.description,
+                "status": self.status,
+                "createdAt": self.createdAt,
+                "updatedAt": self.updatedAt
+            })
         
         json.dump(data,fp=file)
 
@@ -65,38 +71,134 @@ class ListDataModel:
     def add(self,description):
         try:
             # auto increment
-            data, file = self.load_data()
+            data = self.load_data()
+            file = self.write_data()
             self.id = len(data) + 1
             self.description = description
-            self.status = "mark in progress"
+            self.status = "not done"
             self.createdAt = str(datetime.now())
             self.updatedAt = str(datetime.now())
 
             self.save(data,file)
         except json.decoder.JSONDecodeError:
             logger.warning("File is empty .. writing")
+        
+        except ValueError as e:
+            logger.error(e)
             
 
+    def all(self):
+        data = self.load_data()
+        return data
+    
+    def filter(self,status=""):
+        data = self.load_data()
+        result = []
+
+        for dict_ in data:
+            if dict_["status"] == status:
+                result.append(dict_)
+
+        return result
+    
     def update(self,id,description):
-        logger.info(f"data updated id:{id} to {description}")
+        data = self.load_data()
+        arr_index = 0
+        while arr_index < len(data):
+            if data[arr_index]["id"] == int(id):
+                logger.info(f"Get id: {id}")
+                data[arr_index]["description"] = description
+
+                file = self.write_data()
+                self.save(data,file,update=True)
+                logger.info(f"Data updated id:{id} to {description} successfully")
+                break
+            
+            arr_index += 1
+        
 
     def delete(self,id):
-        logger.info(f"data deleted id:{id}")
+        data = self.load_data()
+        arr_index = 0
+        while arr_index < len(data):
+            if data[arr_index]["id"] == int(id):
+                del data[arr_index]
+                file = self.write_data()
+                self.save(data,file,update=True)
+                logger.info(f"Data deleted id:{id} successfully")
+                break
+            
+            arr_index += 1
 
-    def mark_in_progress(self):
-        logger.info("status set to mark in progress")
+    def mark_in_progress(self,id):
+        data = self.load_data()
+        arr_index = 0
+        while arr_index < len(data):
+            if data[arr_index]["id"] == int(id):
+                data[arr_index]["status"] = "mark in progress"
+                file = self.write_data()
+                self.save(data,file,update=True)
+                logger.info(f"Data at id:{id} marked in progress")
+                break
+            
+            arr_index += 1
 
-    def mark_done(self):
-        logger.info("status as to done")
+    def mark_done(self,id):
+        data = self.load_data()
+        arr_index = 0
+        while arr_index < len(data):
+            if data[arr_index]["id"] == int(id):
+                data[arr_index]["status"] = "mark in progress"
+                file = self.write_data()
+                self.save(data,file)
+                logger.info(f"Data at id:{id} marked as done")
+                break
+            
+            arr_index += 1
 
 
 if "__main__" == __name__:
     # command 
+    model = ListDataModel()
     command = sys.argv[1]
     options = sys.argv[2:]
     if command == "add":
-        model = ListDataModel(description=options[0])
-        model.add(description=options[0])
+        try:
+            
+            model.add(description=options[0])
+        except IndexError as e:
+            logger.error("python main.py add [description]")
+
+    elif command == "list":
+        try:
+            
+            if len(options) >= 1:
+                logger.info(f"Filter by {options[0]}: {model.filter(status=options[0])}")
+            else:
+                logger.info(f"Data: {model.all()}")
+        except IndexError as err:
+            logger.error(err)
+
+    elif command in ["help","?"]:
+        model.__doc__()
+
+    elif command == "update" and len(options) == 2:
+        id = options[0]
+        description = options[1]
+        model.update(id=id,description=description)
+
+    elif command == "delete" and len(options) == 1:
+        model.delete(options[0])
+        
+    elif command == "mark-in-progress" and len(options) == 1:
+        model.mark_in_progress(options[0])
+
+    elif command == "mark-done" and len(options) == 1:
+        model.mark_in_progress(options[0])
+
+    else:
+        model.__doc__()
+        
     # elif command == "update":
     #     update(options)
     # elif command == "delete":
