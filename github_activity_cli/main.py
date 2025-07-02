4c6ada90765efaab8ee741e30753892ea3eb780a
blackbox24 <nicardid@gmail.com>
2025-07-02 10:28:58 +0000

    filtering the activity by event type

--- github_activity_cli/main.py
+++ github_activity_cli/main.py
@@ -1,33 +1,85 @@
 import requests
 import sys
 import logging
+import json
 
 
 logging.basicConfig(level=logging.INFO,format="%(levelname)s %(filename)s %(message)s")
 logger = logging.getLogger(__name__)
 
-if len(sys.argv) == 2:
-    username = sys.argv[1]
-    url = f"https://api.github.com/users/{username}/events"
+class GithubActivities:
+    username = ""
+    data = {}
+    events = ""
 
-    logger.info(f"Request: {url}")
+    # def __init__(self,username=""):
+    #     self.username = username
+            
 
-    response = requests.get(url)
+    def __doc__(self):
+        file = open("docs.txt","r")
+        docs = file.read()
+        logger.info(f"Documentation:\n{docs}")
+        file.close()
+        return 0;
 
-    if response.status_code == 200:
-        logger.info(f"Status code: {response.status_code}")
-        logger.info(f"Getting {username} events ...")
-        logger.info(f"Response: \n{response.json()}")
-    elif response.status_code == 404:
-        logger.error(f"Status code: {response.status_code}")
-        logger.error("Resource cannot be found")
-    else:
-        logger.error(f"Status code: {response.status_code}")
-        logger.error("Unable to retreive information")
+    def make_request(self):
+        response = requests.get(f"https://api.github.com/users/{self.username}/events")
+        if response.status_code == 200:
+            logger.info(f"Status code: {response.status_code}")
+            logger.info(f"Successfully retreive {username} events")
+            self.events = response.json()
+    
+        elif response.status_code == 404:
+            logger.error(f"Status code: {response.status_code}")
+            logger.error(f"Username cannot be found {self.username}")
+            sys.exit()
+            
+        else:
+            logger.error(f"Status code: {response.status_code}")
+            logger.error("Unable to retreive information")
+            sys.exit()
 
-else:
-    logger.warning("Must provide username of github account")
-    file = open("docs.txt","r")
-    docs = file.read()
-    logger.info(f"Documentation:\n{docs}")
-    file.close()
+    def all_events(self):
+        # logger.info(f"Retreive all events:\n{self.data}")
+        return self.events
+
+    def filter_event(self,by_type=""):
+        results = []
+        logger.info(f"Filtering type by {by_type}")
+        for x in self.events:
+            if x["type"] == by_type:
+                results.append(x)
+
+        return results
+
+
+if "__main__" ==  __name__:
+    github_activity = GithubActivities()
+    
+    try:
+        username = sys.argv[1]  
+        github_activity.username = username
+        github_activity.make_request()
+
+        if len(sys.argv) > 2:
+            command = sys.argv[2]
+            option = sys.argv[3]
+
+            if command == "filter":
+                results = github_activity.filter_event(option)
+                logger.info(f"Data: {results}")
+        else:
+            results = github_activity.all_events()
+            logger.info(f"Data: {results}")
+
+
+    except IndexError as e:
+        logger.error(e)
+        github_activity.__doc__()
+
+    except:
+        logger.error("Unable to make api call")
+        raise
+
+    
