52ed1dad4a8232cf94a024be15a320bd7bd7cfd2
blackbox24 <nicardid@gmail.com>
2025-07-18 10:38:16 +0000

    update: implemented weight

---
+++ unit_converter/js/weight_converter.js
@@ -0,0 +1,64 @@
+const len_units = ["mg","g"];
+
+const weight_form = document.getElementById("Weight-form");
+const errors = document.getElementById("errors");
+
+const converter_page = document.getElementById("convert-page");
+const result_page = document.getElementById("result-page");
+const from_id = document.getElementById("from_id")
+const result_id = document.getElementById("result_id")
+
+function Convert(from,to,value){
+    let from_index = 0;
+    let to_index = 0;
+    for(var i = 0; i < len_units.length; i++){
+        if(len_units[i] === from){
+            from_index = i;
+            console.log(from_index)
+        }
+        else if(len_units[i] === to){
+            to_index = i;
+            console.log(to_index)
+        }
+    }
+    if(from_index < to_index){
+        return (10 ** (-to_index + from_index)) * value
+    }
+    else if(from_index > to_index){
+        return  (10 ** (from_index - to_index)) * value
+    }
+    return 0;
+}
+
+weight_form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+
+    const unit_from = weight_form.unit_from.value;
+    const unit_to = weight_form.unit_to.value;
+    const weight = weight_form.length_value.value;
+
+    console.log(`Convert from  ${weight} ${unit_from} to ${unit_to}`)
+    try{
+        if(unit_to === unit_from){
+            throw new Error("Units cannot be the same");
+        }
+        let result = Convert(unit_from,unit_to,weight);
+        console.log("Result: " + result);
+
+        converter_page.style.display = "none";
+        result_page.style.display = "block";
+        from_id.innerText = `${weight} ${unit_from}`;
+        result_id.innerText = `${result}${unit_to}`;
+
+    }catch(error){
+        console.error(error)
+        errors.innerText = "Units cannot be the same";
+        errors.style.display = "block";
+        errors.style.color = "red";
+
+        setTimeout(()=>{
+            errors.style.display = "none";
+        },2000)
+    }
+
+})
