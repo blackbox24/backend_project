a69759fd467693c4af468f51c058c063b1e17ffd
blackbox24 <nicardid@gmail.com>
2025-07-31 15:34:00 +0000

    modified models to include category

--- expense_tracker_api/expense/management/commands/seed_db.py
+++ expense_tracker_api/expense/management/commands/seed_db.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Permission
-from expense.models import Expense
+from expense.models import Expense,Category
 
 from guardian.shortcuts import assign_perm
 from django.contrib.auth import get_user_model
-from random import randint
+from random import randint, choice
 
 User = get_user_model();
 
@@ -34,16 +34,29 @@ class Command(BaseCommand):
 
         return user
 
+    def create_category(self,cats):
+        for cat in cats:
+            obj, created = Category.objects.get_or_create(name=cat)
+            if created:
+                self.stdout.write(self.style.SUCCESS(f"Successfully created {cat}"))
+            else:
+                self.stdout.write(self.style.ERROR(f"{cat} Already exists"))
+
     def handle(self, *args, **options):
         # create users
         no_users = randint(4,8)
         no_expenses = randint(3,7)
+        cats = ["Groceries","Leisure","Electronics","Utilities","Clothing","Health","Others"]
+        self.create_category(cats)
 
         for i in range(0,no_users):
             user = self.create_users(username=f"testuser{i}",email=f"testuser{i}@example.com",password="testpass123")
             for x in range(0,no_expenses):
+                category, _ = Category.objects.get_or_create(name=choice(cats))
                 obj = Expense.objects.create(
-                    name=f"test_item{x}",amount=2.00
+                    name=f"test_item{x}",
+                    amount=2.00,
+                    category=category
                 )
 
                 assign_perm("Expense.change_expense",user,obj)
@@ -52,4 +65,4 @@ class Command(BaseCommand):
                 self.stdout.write(self.style.SUCCESS(f"assigned view, change and delete perm for {obj.name} to {user.username}"))
         self.create_users(username="testuser",email="testuser@example.com",password="testpass123",is_superuser=True)
     
-        self.stdout.write(self.style.SUCCESS("Successfully seeded database"))
+        # self.stdout.write(self.style.SUCCESS("Successfully seeded database"))
